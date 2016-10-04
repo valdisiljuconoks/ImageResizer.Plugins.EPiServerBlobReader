83258d3a908ee1077c7d091fe455bf25fc50e6b0
valdisiljuconoks <valdis@VALDIS-PC>
2016-10-05 02:21:09 +0300

    Some clean-ups before publish

--- src/ImageResizer.Plugins.EPiServerBlobReader/HtmlHelperExtensions.cs
+++ src/ImageResizer.Plugins.EPiServerBlobReader/HtmlHelperExtensions.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Specialized;
 using System.Web.Mvc;
 using EPiServer;
 using EPiServer.Core;
@@ -19,42 +18,20 @@ namespace ImageResizer.Plugins.EPiServer
             return helper.ResizeImage(url, width, height);
         }
 
-        public static UrlBuilder ResizeImage(
-            this HtmlHelper helper, string imageUrl, int? width = null, int? height = null)
+        public static UrlBuilder ResizeImage(this HtmlHelper helper, string imageUrl, int? width = null, int? height = null)
         {
-            if (string.IsNullOrEmpty(imageUrl))
+            if(string.IsNullOrEmpty(imageUrl))
                 return new UrlBuilder(string.Empty);
 
             var builder = new UrlBuilder(imageUrl);
 
-            if (width.HasValue)
-                builder.QueryCollection.Add("w", width.Value.ToString());
+            if(width.HasValue)
+                builder.Add("w", width.Value.ToString());
 
-            if (height.HasValue)
-                builder.QueryCollection.Add("h", height.Value.ToString());
+            if(height.HasValue)
+                builder.Add("h", height.Value.ToString());
 
             return builder;
-        }
-    }
-
-    public static class UrlBuilderExtensions
-    {
-        public static UrlBuilder Add(this UrlBuilder target, string key, string value)
-        {
-            if(target == null)
-                throw new ArgumentNullException(nameof(target));
-
-            target.QueryCollection.Add(key, value);
-            return target;
-        }
-
-        public static UrlBuilder Add(this UrlBuilder target, NameValueCollection collection)
-        {
-            if(target == null)
-                throw new ArgumentNullException(nameof(target));
-
-            target.QueryCollection.Add(collection);
-            return target;
         }
     }
 }
---
+++ src/ImageResizer.Plugins.EPiServerBlobReader/UrlBuilderExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Specialized;
+using EPiServer;
+
+namespace ImageResizer.Plugins.EPiServer
+{
+    public static class UrlBuilderExtensions
+    {
+        public static UrlBuilder Add(this UrlBuilder target, string key, string value)
+        {
+            if(target == null)
+                throw new ArgumentNullException(nameof(target));
+
+            target.QueryCollection.Add(key, value);
+            return target;
+        }
+
+        public static UrlBuilder Add(this UrlBuilder target, NameValueCollection collection)
+        {
+            if(target == null)
+                throw new ArgumentNullException(nameof(target));
+
+            target.QueryCollection.Add(collection);
+            return target;
+        }
+    }
+}
